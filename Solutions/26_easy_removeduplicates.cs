1e3477b76fef85d9c1c865e0679a4d711b34dbd8
melihacil <knghtmellomello@gmail.com>
2022-07-27 22:55:45 +0300

    Update 26_easy_removeduplicates.cs

--- Solutions/26_easy_removeduplicates.cs
+++ Solutions/26_easy_removeduplicates.cs
@@ -14,4 +14,30 @@ public class Solution {
         }        
         return count;
     }
+
+    //Two pointer solution
+     public int RemoveDuplicatesTwoPointer(int[] nums) {
+        
+        
+        //Becuase nums is sorted two pointer method could be used
+        //Left and right pointers starts from the first index
+        int leftptr = 1,rightptr =1;
+        
+        
+        //Loop until right pointer reaches the end
+        while (rightptr < nums.Length) {
+            //if its duplicate continue
+            if (nums[rightptr] == nums[rightptr -1]) 
+                rightptr++;
+            else {
+                //If its not a duplicate then change it with left pointer then continue
+                nums[leftptr] = nums[rightptr];
+                leftptr++;
+                rightptr++;                
+            }            
+        }
+        //As we started from 1 leftpointer counted how many non-duplicate items in the array
+        //And we can return this answer.
+        return leftptr;
+    }
 }
