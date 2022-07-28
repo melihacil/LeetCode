995b24d0d88a6035fab9639fc97103c3d1bc7db0
melihacil <knghtmellomello@gmail.com>
2022-07-28 14:06:22 +0300

    Update 8_medium_atoi.cs

--- Solutions/8_medium_atoi.cs
+++ Solutions/8_medium_atoi.cs
@@ -1,18 +1,28 @@
 public class Solution {
-    public int MyAtoi(string s) {        
+    public int MyAtoi(string s) {  
+        //String to integer (C/C++ atoi func)      
         //if (string.IsNullOrEmpty(s)) return 0;
+        /* 
+           Trim and check if the string is empty
+           create answer to be returned
+           and create an int to hold the index 
+        */
         s = s.Trim();
         int sign = 1;
         long answer = 0;
         int index = 0;
-        if (string.IsNullOrEmpty(s)) return 0;        
+        if (string.IsNullOrEmpty(s)) return 0;    
+        //Checking positive or negative sign after trimming    
         if (s[0] == '-' || s[0] == '+')
         {
             sign = s[0] == '+' ? 1 : -1;
             index++;
-        }       
+        }
+        //Starts from one or two then will go on through until 
+        //it is not a digit. A while loop can be used here rather than for       
         for (int i = index; i < s.Length; i++) {
             if (char.IsNumber(s[i])) {
+            //If its a digit add and check if it has passed max value
                 answer = answer * 10 + s[i] - '0';
                 if (answer > int.MaxValue)
                     break;
